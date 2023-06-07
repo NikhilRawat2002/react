6332eacf0071e078be5587b7fab03022a6ac81da
kapilrohilla <kapilrohilla2002@gmail.com>
2023-06-07 13:56:28 +0530

    Layout tag extra laga rkha tha tune

--- src/App.js
+++ src/App.js
@@ -1,42 +1,26 @@
 
 import React from "react";
-import  Layout  from "./Layout";
- import About from "./About";
- import Contact from "./Contact";
- import Home from "./Home";
- import Menu from "./Menu";
- import {  Route, Routes } from "react-router-dom";
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+import Menu from "./Menu";
+import { Route, Routes } from "react-router-dom";
 //  import Banner from './banner.jpeg'
 
 
 
-   function App () {
-   return(
-      <>
-     <Layout/>
-
-
-    
-
-       <div>
-          
-         
-
-           <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/menu" element={<Menu/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-       </Routes> 
-       
-       
-       </div>
-       
+function App() {
+    return (
+        <>
+            <div>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/menu" element={<Menu />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Routes>
+            </div>
         </>
-       
-       
-
-        
     );
 }
 
--- src/Services.jsx
+++ src/Services.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
-const Services =()=>{
-    return(
+const Services = () => {
+    return (
         <>
-        <div>
-            SERVICES
-        </div>
-        
-                
-             
-                <Link to="/home">home</Link><br></br>
-                </>
+            <div>
+                SERVICES
+            </div>
+
+
+
+            <Link to="/home">home</Link><br></br>
+        </>
     )
 
 }
